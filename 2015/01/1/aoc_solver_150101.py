2276e53150520da9c96fa4e4971be7795eb8f958
crecken <you@example.com>
2024-01-12 15:43:12 -0600

    Solved day 1

---
+++ 2015/01/1/aoc_solver_150101.py
@@ -0,0 +1,27 @@
+def main():
+    input_str = get_input()
+    floor, first_down = get_floor(input_str) 
+    return floor, first_down
+
+def get_input() -> str:
+    with open('input.txt', 'r') as f:
+        input_str = f.read()
+    return input_str
+
+def get_floor(input_str):
+    floor = 0
+    first_down = len(input_str)+1
+    for i, char in enumerate(input_str):
+        if char == '(':
+            floor += 1
+        elif char == ')':
+            floor -= 1
+            if i < first_down:
+                first_down = i
+    return floor, first_down + 1
+
+
+if __name__=='__main__':
+    floor, first_down = main()
+    print(floor)
+    print(first_down)
