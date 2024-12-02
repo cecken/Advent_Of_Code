dc1b449aa17c62c7795efbe4cfa49d23eb6fb41c
cecken <ceckenr1@jhu.edu>
2024-12-02 15:44:44 -0600

    finished day 2!

--- 2024/02/app.py
+++ 2024/02/app.py
@@ -1,24 +1,36 @@
+def check_safe(levels: list[int]):
+    mono_down = True
+    mono_up = True
+    diff_max = 0
+    for i in range(1,len(levels)):
+        if levels[i] <= levels[i-1]:
+            mono_up = False
+        if levels[i] >= levels[i-1]:
+            mono_down = False
+        diff = abs(levels[i] - levels[i-1])
+        if diff > diff_max:
+            diff_max = diff
+    monotonic = mono_down or mono_up
+    if monotonic and (diff_max < 4):
+        return True
+    return False
+
+
 def main():
     safe = 0
     with open('input.txt', 'r') as f:
         for idx, line in enumerate(f.readlines()):
-            mono_down = True
-            mono_up = True
-            diff_max = 0
             levels = [int(x) for x in line.split(' ')]
-            for i in range(1,len(levels)):
-                if levels[i] <= levels[i-1]:
-                    mono_up = False
-                if levels[i] >= levels[i-1]:
-                    mono_down = False
-                diff = abs(levels[i] - levels[i-1])
-                if diff > diff_max:
-                    diff_max = diff
-            monotonic = mono_down or mono_up
-            if monotonic and (diff_max < 4):
+            if check_safe(levels):
                 safe += 1
-                print(f'{idx}: {line}')
-                print(f'mono_up: {mono_up}, mono_down: {mono_down}')
+            else:
+                for i in range(len(levels)):
+                    new_levels = levels.copy()
+                    new_levels.pop(i)
+                    if check_safe(new_levels):
+                        safe += 1
+                        break
+
     print(f'Num Safe: {safe}')
 
 
